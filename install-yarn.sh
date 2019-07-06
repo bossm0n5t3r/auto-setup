9f88d181481a49bce3e7bc6bd3c904d0bb3cbfb2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-06 23:41:00 +0900

    Add install-yarn.sh

---
+++ install-yarn.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+c='\e[32m' # Coloured echo (Green)
+r='tput sgr0' #Reset colour after echo
+
+# Installing Yarn
+echo -e "${c}Installing Yarn"; $r
+cd
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
+echo "" >> .zshrc
+echo "# Set Yarn global path" >> .zshrc
+echo -e "export PATH=\"\$(yarn global bin):\$PATH\"" >> .zshrc
+source .zshrc
+( set -x; yarn -v )
+echo -e "${c}Yarn Installed Successfully."; $r

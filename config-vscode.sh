78ab2f046ea76e602a6c1d12d594c1cac0303d8d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-08 13:52:51 +0900

    Update config-vscode.sh

--- config-vscode.sh
+++ config-vscode.sh
@@ -5,6 +5,7 @@ r='tput sgr0' #Reset colour after echo
 
 # Setting Visual Studio Code
 echo -e "${c}Setting Visual Studio Code"; $r
+code
 cp -rp $HOME/gitFolders/dotfiles/.vscode/settings.json $HOME/.config/Code/User/
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension formulahendry.code-runner

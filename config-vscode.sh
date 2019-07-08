0b7aa0c03b9dd628208e45c73c5205018537f1be
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-08 13:16:01 +0900

    Add config-vscode.sh

---
+++ config-vscode.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+c='\e[32m' # Coloured echo (Green)
+r='tput sgr0' #Reset colour after echo
+
+# Setting Visual Studio Code
+echo -e "${c}Setting Visual Studio Code"; $r
+cp -rp $HOME/gitFolders/dotfiles/.vscode/settings.json $HOME/.config/Code/User/
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension formulahendry.code-runner
+code --install-extension ExodiusStudios.comment-anchors
+code --install-extension eamodio.gitlens
+code --install-extension spywhere.guides
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension hoovercj.vscode-power-mode
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension ritwickdey.LiveServer
+echo -e "${c}Visual Studio Code Setting Successfully."; $r

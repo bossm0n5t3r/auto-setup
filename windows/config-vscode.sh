66f1d85fdbf21e7835292d6b60b617d78f5bf854
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-12 03:41:20 +0900

    Add config-vscode.sh for Windows 10

---
+++ windows/config-vscode.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+c='\e[32m' # Coloured echo (Green)
+r='tput sgr0' #Reset colour after echo
+
+# Setting Visual Studio Code
+echo -e "${c}Setting Visual Studio Code"; $r
+code
+read -p "Press ENTER to continue"
+cp -rp ~/Desktop/gitFolders/dotfiles/.vscode/settings.json ~/AppData/Roaming/Code/User/
+# Basic
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension formulahendry.code-runner
+code --install-extension ExodiusStudios.comment-anchors
+code --install-extension eamodio.gitlens
+code --install-extension spywhere.guides
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension hoovercj.vscode-power-mode
+code --install-extension vscode-icons-team.vscode-icons
+# Web
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension ritwickdey.LiveServer
+# Markdown
+code --install-extension yzhang.markdown-all-in-one
+# Python
+code --install-extension ms-python.python
+echo -e "${c}Visual Studio Code Setting Successfully."; $r

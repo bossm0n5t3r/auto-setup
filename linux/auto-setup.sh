300d8d2e918fb2de9027529e7651737074a5bee4
bossm0n5t3r <38637046+bossm0n5t3r@users.noreply.github.com>
2019-10-24 10:17:25 +0900

    Update auto-setup.sh

--- linux/auto-setup.sh
+++ linux/auto-setup.sh
@@ -145,23 +145,25 @@ echo -e "export PATH=\"\$(yarn global bin):\$PATH\"" >> ~/.profile
 echo -e "${c}Yarn Installed Successfully."; $r
 
 # Installing Anaconda
-echo -e "${c}Installing Anaconda"; $r
-cd
-wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
-bash Anaconda3-2019.07-Linux-x86_64.sh
-echo -e "${c}Anaconda Installed Successfully."; $r
+# 
+# echo -e "${c}Installing Anaconda"; $r
+# cd
+# wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+# bash Anaconda3-2019.07-Linux-x86_64.sh
+# echo -e "${c}Anaconda Installed Successfully."; $r
+
+# Deleting Anaconda3-2019.07-Linux-x86_64.sh
+# 
+# echo -e "${c}Deleting Anaconda3-2019.07-Linux-x86_64.sh"; $r
+# cd
+# rm -rf Anaconda3-2019.07-Linux-x86_64.sh
+# echo -e "${c}Anaconda3-2019.07-Linux-x86_64.sh Deleted Successfully!"; $r
 
 # Deleting auto-setup.sh
 echo -e "${c}Deleting auto-setup.sh"; $r
 cd
 rm -rf auto-setup.sh
 echo -e "${c}auto-setup.sh Deleted Successfully!"; $r
-
-# Deleting Anaconda3-2019.07-Linux-x86_64.sh
-echo -e "${c}Deleting Anaconda3-2019.07-Linux-x86_64.sh"; $r
-cd
-rm -rf Anaconda3-2019.07-Linux-x86_64.sh
-echo -e "${c}Anaconda3-2019.07-Linux-x86_64.sh Deleted Successfully!"; $r
 
 # Final Update and Upgrade Command
 echo -e "${c}Updating and upgrading to finish auto-setup script"; $r

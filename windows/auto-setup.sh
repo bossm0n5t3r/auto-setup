22319e796ec7acc46e5eeddef4b86273ef74ee7f
bossm0n5t3r <38637046+bossm0n5t3r@users.noreply.github.com>
2019-11-16 21:27:56 +0900

    Update and rename config-vscode.sh to auto-setup.sh

--- windows/auto-setup.sh
+++ windows/auto-setup.sh
@@ -3,6 +3,17 @@
 c='\e[32m' # Coloured echo (Green)
 r='tput sgr0' #Reset colour after echo
 
+# Setting up Git
+echo -e "${c}Setting up Git"; $r
+( set -x; git --version )
+echo -e "${c}Setting up global git config"; $r
+git config --global color.ui true
+read -p "Enter Your Full Name: " name
+read -p "Enter Your Email: " email
+git config --global user.name "$name"
+git config --global user.email "$email"
+echo -e "${c}Git Setup Successfully!"; $r
+
 # Setting Visual Studio Code
 echo -e "${c}Setting Visual Studio Code"; $r
 code
@@ -33,5 +44,6 @@ code --install-extension tushortz.java-snippets
 code --install-extension tushortz.java-imports-snippets
 # MySQL
 # code --install-extension formulahendry.vscode-mysql
+
 echo -e "${c}Move settings.json to ~/AppData/Roaming/Code/User/."; $r
 echo -e "${c}Visual Studio Code Setting Successfully."; $r

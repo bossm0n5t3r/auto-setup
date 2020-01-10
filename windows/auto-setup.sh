7360e7215ffd08f025ebc6c648711dc6155f1296
bossm0n5t3r <38637046+bossm0n5t3r@users.noreply.github.com>
2020-01-10 21:10:21 +0900

    Update auto-setup.sh

--- windows/auto-setup.sh
+++ windows/auto-setup.sh
@@ -18,32 +18,6 @@ echo -e "${c}Git Setup Successfully!"; $r
 echo -e "${c}Setting Visual Studio Code"; $r
 code
 read -p "Press ENTER to continue"
-# Basic
-code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension formulahendry.code-runner
-code --install-extension ExodiusStudios.comment-anchors
-code --install-extension eamodio.gitlens
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension hoovercj.vscode-power-mode
-code --install-extension VisualStudioExptTeam.vscodeintellicode
-code --install-extension vscode-icons-team.vscode-icons
-code --install-extension wakatime.vscode-wakatime
-# Web
-code --install-extension ritwickdey.LiveServer
-# Prettier
-code --install-extension esbenp.prettier-vscode
-# Markdown
-code --install-extension yzhang.markdown-all-in-one
-# Python
-code --install-extension ms-python.python
-# Java
-code --install-extension redhat.java
-code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-test
-code --install-extension tushortz.java-snippets
-code --install-extension tushortz.java-imports-snippets
-# MySQL
-# code --install-extension formulahendry.vscode-mysql
-
+code --install-extension Shan.code-settings-sync
 echo -e "${c}Move settings.json to ~/AppData/Roaming/Code/User/."; $r
 echo -e "${c}Visual Studio Code Setting Successfully."; $r
